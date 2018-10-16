5bfad88fe1e1d105b14ed2515d57d070f59dd336
pontus <pontus.persson2@elev.ga.lbs.se>
2018-10-16 23:09:42 +0200

    fucking kms

---
+++ Assets/Scripts/WindingScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindingScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    float rotationSpeed = 20f;
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
